759f64c13e2da4d84dd9e0388e6eb73d1001e522
springsama <spring_maprang@hotmail.com>
2015-12-03 23:08:05 +0700

    background2

--- app/build/generated/source/r/debug/com/example/androidclient/R.java
+++ app/build/generated/source/r/debug/com/example/androidclient/R.java
@@ -1693,8 +1693,9 @@ containing a value of this type.
     }
     public static final class mipmap {
         public static final int background=0x7f030000;
-        public static final int ic_launcher=0x7f030001;
-        public static final int logo=0x7f030002;
+        public static final int background2=0x7f030001;
+        public static final int ic_launcher=0x7f030002;
+        public static final int logo=0x7f030003;
     }
     public static final class string {
         public static final int abc_action_bar_home_description=0x7f060000;
